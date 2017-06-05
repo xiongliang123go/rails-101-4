abdb70863c69a4ac8200f35007a18d82325f5d55
熊亮 <xiongliang@xiongliangdeMacBook-Pro.local>
2017-06-05 13:44:59 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:warning] = "这是warning 信息！"
+  end
+
 end
--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -2,10 +2,10 @@ module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
   def flash_class(key)
-    FLASH_CLASSES.fetch key.to-sym, key
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
-    flash.to-hash.slice "alert", "notice", "warning"
+    flash.to_hash.slice "alert", "notice", "warning"
   end
 end 
